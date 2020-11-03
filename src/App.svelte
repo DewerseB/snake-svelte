c097158b911b28b6a8f84d03fd72798985c3b957
Bastien Dewerse <bastien.dewerse@gmail.com>
2020-11-03 17:00:51 +0100

    One shot

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+	import Game from "./components/Game.svelte";
+
+	let game = {
+		width : 600,
+		height : 400,
+		squareSize : 40,
+	};
+
+	function gameInit() {
+		game = {
+			width : 600,
+			height : 400,
+			squareSize : 40,
+		};
+	};
+</script>
+
+<main>
+	<h1>Snake</h1>
+	{#key game}
+		<Game {...game}/>
+	{/key}
+	<button on:click={gameInit}>Restart</button>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+</style>
